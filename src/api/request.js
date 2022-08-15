47b1eca49c81ab01b6142e4a97d7cecd843a2e99
Teayone <1968951189@qq.com>
2022-08-15 16:34:26 +0800

    update

--- src/api/request.js
+++ src/api/request.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { show, hide } from "../plugins/loading";
 const baseURL =
   process.env.NODE_ENV === "production"
     ? "https://music-api-teayone.vercel.app"
@@ -7,25 +6,6 @@ const baseURL =
 export function request(config) {
   const instance = axios.create({
     baseURL,
-    timeout: 5000,
   });
-  instance.interceptors.request.use(
-    (req) => {
-      show();
-      return req;
-    },
-    () => {
-      hide();
-    }
-  );
-  instance.interceptors.response.use(
-    (res) => {
-      hide();
-      return res;
-    },
-    () => {
-      hide();
-    }
-  );
   return instance(config);
 }
